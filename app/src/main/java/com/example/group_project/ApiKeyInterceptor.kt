bcb4c19db718289008838a4eb8dbf7986d2fdbc2
Sriman Ujhanthachhen <srimanp201@gmail.com>
2023-11-12 00:18:17 -0800

    change

--- app/src/main/java/com/example/group_project/ApiKeyInterceptor.kt
+++ app/src/main/java/com/example/group_project/ApiKeyInterceptor.kt
@@ -7,7 +7,7 @@ class ApiKeyInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val newRequest = originalRequest.newBuilder()
-            .addHeader("X-RapidAPI-Key", "5b1ee3b3fdmsh3a24dbd1237204dp14bf21jsn175b41d48588")
+            .addHeader("X-RapidAPI-Key", "API KEY")
             .addHeader("X-RapidAPI-Host", "api-football-v1.p.rapidapi.com")
             .build()
         return chain.proceed(newRequest)
